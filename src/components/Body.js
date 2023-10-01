90577e7dcf51412806bd43f6d45e517910026a6d
munam <munam.ramay99@gmail.com>
2023-10-01 09:33:38 -0700

    assignment..

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,7 +18,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json);
+    console.log('json',json);
     // Optional Chaining
     setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
     setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
